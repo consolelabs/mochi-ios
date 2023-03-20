4f1d67eb8f153e54c059b3c134759859db21dcff
phucledien <phucledien@gmail.com>
2023-03-21 00:51:18 +0700

    fix: update env path

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -9,8 +9,8 @@
 echo "Stage: PRE-Xcode Build is activated .... "
 # This is important because the position of the subsequently mentioned files depend of this origin.
 cd $CI_WORKSPACE/ci_scripts || exit 1
-plutil -replace MORALIS_API_KEY -string $MORALIS_API_KEY ../iOS/Secret.plist
-plutil -p ../iOS/Secret.plist
+plutil -replace MORALIS_API_KEY -string $MORALIS_API_KEY ../iOS/Supports/Secret.plist
+plutil -p ../iOS/Supports/Secret.plist
 echo "Stage: PRE-Xcode Build is DONE .... "
 
 exit 0

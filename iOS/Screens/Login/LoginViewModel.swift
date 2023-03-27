ebec3a19e2ada27ff7ef9845bc4218ec11f6713f
phucledien <phucledien@gmail.com>
2023-03-27 19:41:58 +0700

    feat: metamask auth

--- iOS/Screens/Login/LoginViewModel.swift
+++ iOS/Screens/Login/LoginViewModel.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import OSLog
 import Combine
+import metamask_ios_sdk
 
 enum MochiError: Error {
   case custom(String)
@@ -15,15 +16,27 @@ enum MochiError: Error {
 
 @MainActor
 class LoginViewModel: ObservableObject {
- 
+
+  // MARK: Public states
   var openURL = PassthroughSubject<URL, Never>()
   var accessToken = PassthroughSubject<String, Never>()
   
+  // MARK: Private states
+  private let authCode = String.random(length: 5)
+  private var signMessage: String {
+    return "This will help us connect your discord account to the wallet address.\n\nMochiBotCode=\(authCode)"
+  }
+  private var cancellables: Set<AnyCancellable> = []
+
+
+  // MARK: Services
+  @ObservedObject
+  private var ethereum = MetaMaskSDK.shared.ethereum
   private let phantomWalletService = PhantomWalletService()
   private let mochiProfileService = MochiProfileServiceImp(keychainService: KeychainServiceImpl())
-  
   private let logger = Logger(subsystem: "so.console.mochi", category: "LoginViewModel")
-  private let authCode = String.random(length: 5)
+ 
+  // MARK: - Public methods
   
   func onOpenURL(url: URL) {
     guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
@@ -41,13 +54,59 @@ class LoginViewModel: ObservableObject {
     }
   }
   
+  func loginWithMetamask() {
+    guard !ethereum.connected else {
+      signMessageMetamaskWallet()
+      return
+    }
+    
+    let dapp = Dapp(name: "Mochi", url: "https://mochi.gg")
+    // This is the same as calling "eth_requestAccounts"
+    ethereum.connect(dapp)?
+      .receive(on: DispatchQueue.main)
+      .sink(receiveCompletion: { [weak self] completion in
+      switch completion {
+      case let .failure(error):
+        self?.logger.error("connect to metamask error: \(error)")
+      default: break
+      }
+    }, receiveValue: { [weak self] result in
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        self?.signMessageMetamaskWallet()
+      }
+    }).store(in: &cancellables)
+  }
+  
   func loginWithPhantom() {
     let appURL = "https://mochi.gg"
     let redirectLink = "so.console.mochi://onPhantomConnected"
     guard let deeplink = try? phantomWalletService.getDeeplink(for: .connect(appURL: appURL, redirectLink: redirectLink)) else {return}
     openURL.send(deeplink)
   }
+  
+  // MARK: - Private methods
+
+  private func signMessageMetamaskWallet() {
+    let from = ethereum.selectedAddress
+    let params = [signMessage, from]
+    let signRequest = EthereumRequest(
+      method: .personalSign,
+      params: params
+    )
+    ethereum.request(signRequest)?.sink(receiveCompletion: { [unowned self] completion in
+      switch completion {
+      case let .failure(error):
+        self.logger.error("sign message metamask failed, error: \(error)")
+      default: break
+      }
+    }, receiveValue: { [weak self] signature in
+      guard let self, let signature = signature as? String else {return}
+      self.authWithEVM(code: self.authCode, signature: signature, walletAddress: from)
+    }).store(in: &cancellables)
     
+    self.openURL.send(URL(string: "https://metamask.app.link")!)
+  }
+  
   private func onPhantomConnected(queryItems: [URLQueryItem]) {
     do {
        _ = try phantomWalletService.getPhantomConnectData(queryItems: queryItems)
@@ -68,9 +127,8 @@ class LoginViewModel: ObservableObject {
   }
   
   private func signMessagePhantomWallet() {
-    let message = "This will help us connect your discord account to the wallet address.\n\nMochiBotCode=\(authCode)"
     let redirectLink = "so.console.mochi://onPhantomSignedMessage"
-    guard let deeplink = try? phantomWalletService.getDeeplink(for: .sign(message: message, redirectLink: redirectLink)) else {return}
+    guard let deeplink = try? phantomWalletService.getDeeplink(for: .sign(message: signMessage, redirectLink: redirectLink)) else {return}
     openURL.send(deeplink)
   }
   
@@ -82,6 +140,18 @@ class LoginViewModel: ObservableObject {
         self.accessToken.send(resp.data.accessToken)
       case .failure(let error):
         logger.error("mochiProfileService.authBySolana failed, code: \(code), signature: \(signature), walletAddrres: \(walletAddress), error: \(error)")
+      }
+    }
+  }
+  
+  private func authWithEVM(code: String, signature: String, walletAddress: String) {
+    Task(priority: .high) {
+      let result = await mochiProfileService.authByEVM(code: code, signature: signature, walletAddress: walletAddress)
+      switch result {
+      case .success(let resp):
+        self.accessToken.send(resp.data.accessToken)
+      case .failure(let error):
+        logger.error("mochiProfileService.authByEVM failed, code: \(code), signature: \(signature), walletAddrres: \(walletAddress), error: \(error)")
       }
     }
   }
